87ed4866af695ee88822c1eaefe22c33b6d31db3
wtbd <2654201663@qq.com>
2023-02-11 00:33:37 +0800

    hello commit

--- src/main/java/com/wu/controller/test.java
+++ src/main/java/com/wu/controller/test.java
@@ -7,5 +7,6 @@ import java.sql.*;
 public class test {
     public static void main(String[] args) throws SQLException {
         System.out.println("hello.txt");
+        System.out.println("hello234");
     }
 }
